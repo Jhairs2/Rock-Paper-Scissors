fbed21914e6f23cdd393709b2871c43abf85285a
Justin Hairston <jhairst2@vols.utk.edu>
2022-08-31 15:46:12 -0400

    Reformat all code files, add scoring system to game

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,16 @@
-let playerChoice = "" ;
+let playerChoice = "";
+
+
+
 // Create variables to change Player image
 const playerPic = document.querySelector(".player-choice-pic");
 const cpuPic = document.querySelector(".cpu-choice-pic");
 const btn = document.querySelectorAll("button");
 const results = document.querySelector('.result-message');
+const player = document.querySelector(".player-score");
+const cpu = document.querySelector(".cpu-score");
+let playerScore = 0;
+let cpuScore = 0;
 
 // Set pictures to default
 playerPic.src = "./images/rock.jpg";
@@ -13,7 +20,7 @@ cpuPic.src = "./images/random.svg";
 // Get cpu's choice to compare with user
 // Get random number from 1-3
 // cpu image will change with choice
-function changeCpuChoice() { 
+function changeCpuChoice() {
 
     let cpuChoice = Math.floor(Math.random() * 3);
     let choice = ["Rock", "Paper", "Scissors"];
@@ -21,43 +28,43 @@ function changeCpuChoice() {
     if (choice[cpuChoice] == "Rock") {
         cpuPic.src = "./images/rock.jpg";
         return choice = "Rock";
-    } 
+    }
     else if (choice[cpuChoice] == "Paper") {
         cpuPic.src = "./images/paper.svg";
         return choice = "Paper";
-        
+
     }
     else if (choice[cpuChoice] == "Scissors") {
         cpuPic.src = "./images/scissors.svg";
-        return choice = "Scissors";      
+        return choice = "Scissors";
     }
 }
 
 // btn will change player image on click
-    Array.from(btn).forEach(function(btn) {
-    btn.addEventListener('click', function() {
-      console.log(btn.textContent);
-      ;  
-      
-    if (btn.textContent == "Rock") {
-        playerPic.src = "./images/rock.jpg"
-        playerChoice = "Rock";
-        return game();
-    } 
-    else if (btn.textContent == "Paper") {
-        playerPic.src = "./images/paper.svg";
-         playerChoice = "Paper";
-         return game();
-    }
-    else if (btn.textContent == "Scissors") {
-        playerPic.src = "./images/scissors.svg";
-         playerChoice = "Scissors";
-         return game();
-    };
+Array.from(btn).forEach(function (btn) {
+    btn.addEventListener('click', function () {
+        console.log(btn.textContent);
+        ;
+
+        if (btn.textContent == "Rock") {
+            playerPic.src = "./images/rock.jpg"
+            playerChoice = "Rock";
+            return game();
+        }
+        else if (btn.textContent == "Paper") {
+            playerPic.src = "./images/paper.svg";
+            playerChoice = "Paper";
+            return game();
+        }
+        else if (btn.textContent == "Scissors") {
+            playerPic.src = "./images/scissors.svg";
+            playerChoice = "Scissors";
+            return game();
+        };
 
     })
-    
-    ;
+
+        ;
 })
 
 
@@ -65,26 +72,38 @@ function changeCpuChoice() {
 function playRound(playerSelection, computerSelection) {
     // If player chooses Rock
     if (playerSelection == "Rock" && computerSelection == "Scissors") {
+        playerScore++;
+        player.textContent = playerScore;
         return "You Win! Rock beats Scissors!";
     }
     else if (playerSelection == "Rock" && computerSelection == "Paper") {
+        cpuScore++;
+        cpu.textContent = cpuScore;
         return "You Lose! Paper beats Rock!";
     }
 
     // If Player chooses Paper
     else if (playerSelection == "Paper" && computerSelection == "Rock") {
+        playerScore++;
+        player.textContent = playerScore;
         return "You Win! Paper beats Rock!";
     }
     else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+        cpuScore++;
+        cpu.textContent = cpuScore;
         return "You Lose! Scissors beats Paper!";
     }
 
 
     // If player chooses Scissors
     if (playerSelection == "Scissors" && computerSelection == "Paper") {
+        playerScore++;
+        player.textContent = playerScore;
         return "You Win! Scissors beats Paper!";
     }
     else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+        cpuScore++;
+        cpu.textContent = cpuScore;
         return "You Lose! Rock beats Scissors!";
     }
 
@@ -96,14 +115,11 @@ function playRound(playerSelection, computerSelection) {
 }
 
 // Game will be simulated
-function game() {;
+function game() {
+    ;
     return results.textContent = playRound(playerChoice, changeCpuChoice())
 }
 
-
-
-/* let playerScore = 0;
-let cpuScore = 0;
 
 
 
